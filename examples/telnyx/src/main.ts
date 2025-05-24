714adb740ea909b0e975340763ce5d093ca98121
Adam Patterson <adam@farar.net>
2025-05-23 21:23:39 -0400

    more

--- examples/telnyx/src/main.ts
+++ examples/telnyx/src/main.ts
@@ -43,7 +43,7 @@ const controller = new TelnyxController({
 });
 
 controller.on("init", (voip: VoIP) => {
-  const stt = new DeepgramSTT({ apiKey: DEEPGRAM_API_KEY, openAIAPIKey: OPENAI_API_KEY });
+  const stt = new DeepgramSTT({ apiKey: DEEPGRAM_API_KEY });
   const tts = new CartesiaTTS({ apiKey: CARTESIA_API_KEY });
   const agent = new OpenAIAgent({ apiKey: OPENAI_API_KEY, system: OPENAI_SYSTEM_MESSAGE, greeting: OPENAI_GREETING_MESSAGE });
   const dialog = new Dialog({ voip, stt, tts, agent });
--- examples/twilio/src/main.ts
+++ examples/twilio/src/main.ts
@@ -41,7 +41,7 @@ const controller = new TwilioController({
 });
 
 controller.on("init", (voip: VoIP) => {
-  const stt = new DeepgramSTT({ apiKey: DEEPGRAM_API_KEY, openAIAPIKey: OPENAI_API_KEY });
+  const stt = new DeepgramSTT({ apiKey: DEEPGRAM_API_KEY });
   const tts = new CartesiaTTS({ apiKey: CARTESIA_API_KEY });
   const agent = new OpenAIAgent({ apiKey: OPENAI_API_KEY, system: OPENAI_SYSTEM_MESSAGE, greeting: OPENAI_GREETING_MESSAGE });
   const dialog = new Dialog({ voip, stt, tts, agent });
