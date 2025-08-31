f1556acb46c4373e56686a27b041cc0acec2bcb7
Adam Patterson <adam@farar.net>
2025-08-31 17:29:56 +0000

    more

--- src/implementations/voip/twilio/twilio_voip_proxy.ts
+++ src/implementations/voip/twilio/twilio_voip_proxy.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { EventEmitter } from "node:events";
 import { VoIP, VoIPEvents } from "../../../interfaces/voip/voip.js";
 import { Agent } from "port_agent";
