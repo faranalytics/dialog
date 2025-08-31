00e2bceab30abb74b798e38f7dd20b329335011d
Adam Patterson <adam@farar.net>
2025-08-31 17:38:52 +0000

    more

---
+++ examples/twilio_threading/src/agent.ts
@@ -0,0 +1,34 @@
+import { CartesiaTTS, ElevenlabsTTS, log, Message, TwilioVoIPOpenAIAgent } from "@farar/dialog";
+import { CARTESIA_API_KEY, CARTESIA_SPEECH_OPTIONS, ELEVEN_LABS_API_KEY } from "./settings.js";
+
+export class Agent extends TwilioVoIPOpenAIAgent {
+  public inference = async (message: Message): Promise<void> => {
+    try {
+      if (message.data.includes("agent")) {
+        if (this.tts instanceof CartesiaTTS) {
+          this.setTTS(new ElevenlabsTTS({ apiKey: ELEVEN_LABS_API_KEY }));
+        }
+        else if (this.tts instanceof ElevenlabsTTS) {
+          this.setTTS(new CartesiaTTS({ apiKey: CARTESIA_API_KEY, speechOptions: CARTESIA_SPEECH_OPTIONS }));
+        }
+        else {
+          throw new Error("Unhandled setTTS.");
+        }
+      }
+      log.notice(`User message: ${message.data}`);
+      this.history.push({ role: "user", content: message.data });
+      const stream = await this.openAI.chat.completions.create({
+        model: this.model,
+        messages: this.history,
+        temperature: 1,
+        stream: true
+      });
+      const assistantMessage = await this.dispatchStream(message.uuid, stream);
+      log.notice(`Assistant message: ${assistantMessage} `);
+      this.history.push({ role: "assistant", content: assistantMessage });
+    }
+    catch (err) {
+      this.dispose(err);
+    }
+  };
+}
---
+++ examples/twilio_threading/src/worker.ts
@@ -0,0 +1,30 @@
+import { Agent } from "./agent.js";
+import { CartesiaTTS, DeepgramSTT, TwilioVoIPProxy, } from "@farar/dialog";
+import {
+  DEEPGRAM_API_KEY,
+  DEEPGRAM_LIVE_SCHEMA,
+  CARTESIA_API_KEY,
+  CARTESIA_SPEECH_OPTIONS,
+  OPENAI_API_KEY,
+  OPENAI_SYSTEM_MESSAGE,
+  OPENAI_GREETING_MESSAGE,
+  OPENAI_MODEL,
+  TWILIO_ACCOUNT_SID,
+  TWILIO_AUTH_TOKEN
+} from "./settings.js";
+
+const voip = new TwilioVoIPProxy();
+
+const agent = new Agent({
+  voip: voip,
+  stt: new DeepgramSTT({ apiKey: DEEPGRAM_API_KEY, liveSchema: DEEPGRAM_LIVE_SCHEMA }),
+  tts: new CartesiaTTS({ apiKey: CARTESIA_API_KEY, speechOptions: CARTESIA_SPEECH_OPTIONS }),
+  apiKey: OPENAI_API_KEY,
+  system: OPENAI_SYSTEM_MESSAGE,
+  greeting: OPENAI_GREETING_MESSAGE,
+  model: OPENAI_MODEL,
+  twilioAccountSid: TWILIO_ACCOUNT_SID,
+  twilioAuthToken: TWILIO_AUTH_TOKEN,
+});
+
+agent.activate();
